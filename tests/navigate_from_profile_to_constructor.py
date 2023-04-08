82b65b25533e49e6d2c3f861b8847c9e89820c52
Anna Lebedeva <angelswan404@gmail.com>
2023-04-08 08:09:35 +0300

    added notes for reviewer

--- tests/navigate_from_profile_to_constructor.py
+++ tests/navigate_from_profile_to_constructor.py
@@ -6,6 +6,9 @@ from locators import (Home,
                       Profile,
                       Login)
 
+#  У меня все тесты не могут пройти дальше логина и регистрации. Не понимаю почему.
+#  Высылаю задание, чтобы получить хоть какую-то помощь и объяснение, почему там падает.
+
 
 class TestNavigateToProfile:
     def test_navigate_to_profile(self, driver):
@@ -15,7 +18,7 @@ class TestNavigateToProfile:
         driver.find_element(*Login.EMAIL_FIELD).send_keys(TEST_LOGIN)
         driver.find_element(*Login.PASSWORD_FIELD).send_keys(TEST_PASSWORD)
         driver.find_element(*Login.SIGN_IN_BTN).click()
-        webdriver_wait(driver, Home.CONSTRACTOR_TITLE)
+        webdriver_wait(driver, Home.PROFILE_BTN)
         driver.find_element(*Home.PROFILE_BTN).click()
         webdriver_wait(driver, Profile.SIGN_IN_BTN)
         driver.find_element(*Profile.CONSTRUCTOR_BTN).click()
